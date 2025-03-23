f7dda07a2ae48296dc9d344d802b83481b38a6cf
YuGal-69 <yugalhemane@gmail.com>
2025-03-23 13:38:33 +0530

    update pg

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-description',
-  imports: [],
+  standalone: true, // Ensure it's a standalone component
   templateUrl: './product-description.component.html',
-  styleUrl: './product-description.component.scss'
+  styleUrls: ['./product-description.component.scss'],
+  imports: [CommonModule], // Import CommonModule to use *ngIf
 })
 export class ProductDescriptionComponent {
+  isDropdownOpen = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+
+  toggleDropdown(event: Event) {
+    event.stopPropagation();
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  closeDropdown(event: Event) {
+    if (this.isDropdownOpen) {
+      this.isDropdownOpen = false;
+    }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      document.addEventListener('click', (event) => this.closeDropdown(event));
+    }
+  }
 }
